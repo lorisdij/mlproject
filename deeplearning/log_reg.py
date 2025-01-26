ce4543a59d8600ae0cff4c6841fe0dd805e5d9ae
Loris <lorisroy39@gmail.com>
2025-01-26 17:54:15 +0100

    ui

---
+++ deeplearning/log_reg.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def get_dataset():
+    """
+        Method used to generate the dataset
+    """
+    #number of row per class
+    row_per_class=10
+    print(np.random.randn(row_per_class,2)).shape
+
+
+
+
+if __name__=='__main__':
+    get_dataset()
