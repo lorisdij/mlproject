77cb6b5dfe98af70f59888aa9138fed0688cae38
Loris <lorisroy39@gmail.com>
2025-01-28 19:52:29 +0100

    ee

--- deeplearning/log_reg.py
+++ deeplearning/log_reg.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 """
     Objectif : créer un algorithme capable de prédire qui est malade(1)/non-malade(0) à partir de données x1 x2 en ajustant le poids des coefficient à l'aide d'une descente du gradient
@@ -20,7 +22,7 @@ def dataset():
         generate dataset : taux de protéine et glucides dans l'alimentation
     '''
     ligne_par_class=10
-    healthy=np.random.randn(ligne_par_class,2)+[5,5]
+    healthy=np.random.randn(ligne_par_class,2)+[3,3]
     sick=np.random.randn(ligne_par_class,2)
     features=np.vstack([healthy,sick])
 
@@ -50,6 +52,15 @@ def activation(z):
     print(y)
     return y
 
+def derivate_activation(z):
+    '''
+        deriver l'activation (sigmoid)
+    '''
+    derivate_activation=activation(z) * (1 - activation(z))
+    print('--derivate_activation--')
+    print(derivate_activation)
+
+
 def predict(y):
     '''
         On obtient en sortie ce qui est prédit donc 0 : non-malade ou 1 : malade (en arrondissant la resultat de la fonction d'activation)
@@ -60,9 +71,33 @@ def predict(y):
     return prediction
 
 
+def cost(predictions, targets):
+    '''
+        calcul des erreurs pour pouvoir ajuster le poids des modèles
+    '''
+    cost=np.mean((predictions-targets)**2)
+    print('--cost--')
+    print(cost)
+
+
+def train(features,targets,weights,bias,predictions):
+    '''
+        entraînement pour adapter les weights et les bias
+    '''
+    epochs=150
+    learning_rate=0.1
+
+    print(f'Accuracy = {np.mean(predictions==targets)}')
+    plt.scatter(features[:, 0], features[:, 1], s=40, c=targets, cmap=plt.cm.Spectral)
+    plt.show()
+
+
 if __name__=='__main__':
     features, targets = dataset()
     bias, weights = init_variables()
     z=pre_activation(features,weights,bias)
     y=activation(z)
-    predict(y)
+    predictions=predict(y)
+    derivate_activation(z)
+    cost(predictions,targets)
+    train(features,targets,weights,bias,predictions)
